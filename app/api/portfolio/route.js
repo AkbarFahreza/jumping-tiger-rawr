1339744833e904d55d6c1061d95d214d10a65520
Akbar Fahreza <66171523+AkbarFahreza@users.noreply.github.com>
2023-11-26 05:49:20 +0700

    Update route.js

--- app/api/portfolio/route.js
+++ app/api/portfolio/route.js
@@ -9,9 +9,7 @@ const items = [
         image:
           "https://res.cloudinary.com/dxcmt3zoc/image/upload/v1700558447/FzFnjyUagAA1PCz_lpde9y.jpg",
         sourceLink: "https://x.com/Revernry/status/1671230836610572289?s=20",
-        ytLink: "https://www.youtube.com/@NanamiChia",
-        xLink: "https://twitter.com/NanamiChia13RP",
-        category: "commission",
+        category: "YT Chat Commissions",
         Datecreated: "June 2023",
       },
       {
@@ -20,9 +18,7 @@ const items = [
         image:
           "https://res.cloudinary.com/dxcmt3zoc/image/upload/v1700558869/FzYyB61akAAJB-v_b3uisg.jpg",
         sourceLink: "https://x.com/Revernry/status/1672579350674444288?s=20",
-        ytLink: "https://www.youtube.com/@nekonyanaimi",
-        xLink: "https://twitter.com/NekonyanAimi",
-        category: "commission",
+        category: "YT Chat Commissions",
         Datecreated: "June 2023",
       },
       {
@@ -31,9 +27,7 @@ const items = [
         image:
           "https://res.cloudinary.com/dxcmt3zoc/image/upload/v1700559109/F0uoR6paYAAoOpU_e0bzbj.jpg",
         sourceLink: "https://x.com/Revernry/status/1678620351830568960?s=20",
-        ytLink: "https://www.youtube.com/@DYMAXVtuber",
-        xLink: "https://twitter.com/DYMAXVtuber",
-        category: "commission",
+        category: "YT Chat Commissions",
         Datecreated: "July 2023",
       },
       {
@@ -42,9 +36,7 @@ const items = [
         image:
           "https://res.cloudinary.com/dxcmt3zoc/image/upload/v1700559599/F1JBHMWaUAETYD2_ycojzs.jpg",
         sourceLink: "https://x.com/Revernry/status/1680477239748755456?s=20",
-        ytLink: "https://www.youtube.com/@NarumiEiko",
-        xLink: "https://twitter.com/NarumiEiko",
-        category: "commission",
+        category: "YT Chat Commissions",
         Datecreated: "July 2023",
       },
       {
@@ -54,8 +46,7 @@ const items = [
           "https://pbs.twimg.com/media/F14c4mraEAAyu_h?format=jpg&name=medium",
         sourceLink: "https://x.com/Revernry/status/1683815123906097155?s=20",
         ytLink: "https://www.youtube.com/@Aisu_rain",
-        xLink: "https://twitter.com/aisu_rain",
-        category: "commission",
+        category: "YT Chat Commissions",
         Datecreated: "July 2023",
       },
       {
@@ -64,19 +55,105 @@ const items = [
         image:
           "https://pbs.twimg.com/media/F2hs-FzakAAAjiU?format=jpg&name=medium",
         sourceLink: "https://x.com/Revernry/status/1686717913271554048?s=20",
-        ytLink: "https://www.youtube.com/@wintergea",
-        xLink: "https://twitter.com/wintergea",
-        category: "commission",
+        category: "YT Chat Commissions",
         Datecreated: "August 2023",
+      },
+      {
+        id: 7,
+        name: "Castella Eri",
+        image:
+          "https://pbs.twimg.com/media/F2q0JjtbwAAyyiG?format=jpg&name=medium",
+        sourceLink: "https://x.com/Revernry/status/1687359118053662720?s=20",
+        category: "YT Chat Commissions",
+        Datecreated: "August 2023",
+      },
+      {
+        id: 8,
+        name: "Ade Deviela Ch",
+        image:
+          "https://pbs.twimg.com/media/F3aFwAKbYAAUdm8?format=jpg&name=medium",
+        sourceLink: "https://x.com/Revernry/status/1690685799514431488?s=20",
+        category: "YT Chat Commissions",
+        Datecreated: "August 2023",
+      },
+      {
+        id: 9,
+        name: "Mahiru Rui",
+        image:
+          "https://pbs.twimg.com/media/F4g2arOasAAZPoo?format=jpg&name=medium",
+        sourceLink: "https://x.com/Revernry/status/1695665154455970240?s=20",
+        category: "YT Chat Commissions",
+        Datecreated: "August 2023",
+      },
+      {
+        id: 10,
+        name: "Yukari Blackheart Ch.",
+        image:
+          "https://pbs.twimg.com/media/F6CbRMWbEAAYqjD?format=jpg&name=medium",
+        sourceLink: "https://x.com/Revernry/status/1702531433225670664?s=20",
+        category: "YT Chat Commissions",
+        Datecreated: "September 2023",
+      },
+      {
+        id: 11,
+        name: " Taki Takahiro ",
+        image:
+          "https://pbs.twimg.com/media/F6iPu5oaYAAjPSz?format=jpg&name=medium",
+        sourceLink: "https://x.com/Revernry/status/1704770531973808208?s=20",
+        category: "YT Chat Commissions",
+        Datecreated: "September 2023",
+      },
+      {
+        id: 12,
+        name: "Selene Aria",
+        image:
+          "https://pbs.twimg.com/media/F6mnBoea0AAmOL0?format=jpg&name=medium",
+        sourceLink: "https://x.com/Revernry/status/1705077641546707234?s=20",
+        category: "YT Chat Commissions",
+        Datecreated: "September 2023",
+      },
+      {
+        id: 13,
+        name: "Céline Leonhart",
+        image:
+          "https://pbs.twimg.com/media/F7CFKOFaUAAU32d?format=jpg&name=medium",
+        sourceLink: "https://x.com/Revernry/status/1707010711703138495?s=20",
+        category: "YT Chat Commissions",
+        Datecreated: "September 2023",
+      },
+      {
+        id: 14,
+        name: "Ayana Shan Elise Ch.",
+        image:
+          "https://pbs.twimg.com/media/F7GPGu0b0AAc5dW?format=jpg&name=medium",
+        sourceLink: "https://x.com/Revernry/status/1707303127890936284?s=20",
+        category: "YT Chat Commissions",
+        Datecreated: "September 2023",
+      },
+      {
+        id: 15,
+        name: "Seina Ginevra Ch.",
+        image:
+          "https://pbs.twimg.com/media/F8eoN7Ya0AAp3fD?format=jpg&name=medium",
+        sourceLink: "https://x.com/Revernry/status/1713523180261048648?s=20",
+        category: "YT Chat Commissions",
+        Datecreated: "Oktober 2023",
+      },
+      {
+        id: 16,
+        name: "Eliza Hemlock Ch.",
+        image:
+          "https://pbs.twimg.com/media/F9NAF2HaAAAe2on?format=jpg&name=medium",
+        sourceLink: "https://x.com/Revernry/status/1716786883265806588?s=20",
+        category: "YT Chat Commissions",
+        Datecreated: "",
       },
       // {
       //   id: 5,
       //   name: "",
       //   image: "",
       //   sourceLink: "",
-      //   ytLink: "",
-      //   xLink: "",
-      //   category: "commission",
+      //   category: "YT Chat Commissions",
       //   Datecreated: "",
       // },
       // Add more items as needed
