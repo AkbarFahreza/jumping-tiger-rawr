42262e41471c1c64f409f2f2369923a70a8df097
reza <reza@pop-os.localdomain>
2023-11-27 03:08:28 +0700

    add portfolio page

---
+++ app/comms-info/page.jsx
@@ -0,0 +1,45 @@
+"use client";
+import React from "react";
+import { useState } from "react";
+import PriceList from "@/components/comms-info/PriceList";
+import TermOfServices from "@/components/comms-info/TermOfServices";
+
+function Page() {
+  const [activeTab, setActiveTab] = useState(1);
+
+  const handleTabButton = (buttonName) => {
+    setActiveTab(buttonName);
+  };
+
+  return (
+    <div className="relative mobile-container">
+      <div className="flex flex-row justify-between items-center  my-6">
+        <p className="mobile-heading-b text-white">
+          Status : <span className="text-green-500">Open</span>
+        </p>
+        <div className="flex flex-row space-x-3">
+          <button
+            className={`py-1 px-3 border-2 border-white text-white mobile-subtitle-b rounded-full ${
+              activeTab === 1 ? "bg-pink" : "bg-secondary"
+            } text-white`}
+            onClick={() => handleTabButton(1)}
+          >
+            PriceList
+          </button>
+          <button
+            className={`py-1 px-3 border-2 border-white text-white mobile-subtitle-b rounded-full ${
+              activeTab === 2 ? "bg-pink" : "bg-secondary"
+            } text-white`}
+            onClick={() => handleTabButton(2)}
+          >
+            TOS
+          </button>
+        </div>
+      </div>
+      {activeTab === 1 && <PriceList />}
+      {activeTab === 2 && <TermOfServices />}
+    </div>
+  );
+}
+
+export default Page;
---
+++ app/portfolio/page.jsx
@@ -0,0 +1,136 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+function HomePortfolio() {
+  const [items, setItems] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [activeCategory, setActiveCategory] = useState(null);
+  const [originalDataLength, setOriginalDataLength] = useState(0);
+  const [loading, setLoading] = useState(true);
+  const [sortOrder, setSortOrder] = useState("asc"); // Default to ascending order
+
+  useEffect(() => {
+    async function fetchItems() {
+      try {
+        const response = await fetch("/api/portfolio");
+        const data = await response.json();
+        setOriginalDataLength(data[0].data);
+        setItems(data[0].data);
+
+        // Extract unique categories from the data
+        const uniqueCategories = Array.from(
+          new Set(data[0].data.map((item) => item.category))
+        );
+        setCategories(uniqueCategories);
+
+        // Set the default active category (you can modify this logic as per your requirements)
+        setActiveCategory(
+          uniqueCategories.length > 0 ? uniqueCategories[0] : null
+        );
+      } catch (error) {
+        console.error("Error fetching items:", error);
+      }
+    }
+
+    fetchItems();
+  }, []);
+
+  const handleTabClick = (category) => {
+    setActiveCategory(category);
+  };
+
+  const toggleSortOrder = () => {
+    setSortOrder((prevOrder) => (prevOrder === "asc" ? "desc" : "asc"));
+  };
+
+  // Group items first by category and then by Datecreated
+  const groupedItems = items.reduce((grouped, item) => {
+    const category = item.category;
+    const date = item.Datecreated;
+
+    if (!grouped[category]) {
+      grouped[category] = {};
+    }
+
+    if (!grouped[category][date]) {
+      grouped[category][date] = [];
+    }
+
+    grouped[category][date].push(item);
+    return grouped;
+  }, {});
+
+  const sortDates = (dates) => {
+    return dates.sort((a, b) => {
+      const dateA = new Date(a);
+      const dateB = new Date(b);
+
+      if (sortOrder === "asc") {
+        return dateA - dateB;
+      } else {
+        return dateB - dateA;
+      }
+    });
+  };
+
+  return (
+    <div className="flex flex-col mobile-container pb-20">
+      <div className="mt-2 flex  flex-row space-x-4 ">
+        {categories.map((category) => (
+          <button
+            key={category}
+            onClick={() => handleTabClick(category)}
+            className={` ${activeCategory === category ? "active-tab" : "tab"}`}
+          >
+            {category}
+          </button>
+        ))}
+        <button onClick={toggleSortOrder}>
+          {sortOrder === "asc" ? "Sort Descending" : "Sort Ascending"}
+        </button>
+      </div>
+
+      <div className="mt-4">
+        {/* Render content for the active category */}
+        {groupedItems[activeCategory] &&
+          Object.entries(groupedItems[activeCategory])
+            .sort(([dateA], [dateB]) => {
+              const dateArray = sortDates([dateA, dateB]);
+              return dateArray[0] === dateA ? -1 : 1;
+            })
+            .map(([date, itemsForDate]) => (
+              <div key={date} className="mb-4">
+                <h2 className="font-bold text-white">{date}</h2>
+                <div className="flex flex-wrap justify-center gap-3">
+                  {/* Render items for the current date */}
+                  {itemsForDate.map((item) => (
+                    <Link
+                      href={item.sourceLink}
+                      key={item.id}
+                      className="flex flex-col w-[47%] sm:w-1/2 md:w-1/3 lg:w-1/3 xl:w-1/3"
+                    >
+                      <Image
+                        width={200}
+                        height={200}
+                        alt={item.name}
+                        className="w-full h-auto rounded-md"
+                        src={item.image}
+                      />
+                      <div className="mt-2">
+                        <h1 className="font-bold mobile-subtitle-b text-white">
+                          {item.name}
+                        </h1>
+                      </div>
+                    </Link>
+                  ))}
+                </div>
+              </div>
+            ))}
+      </div>
+    </div>
+  );
+}
+
+export default HomePortfolio;
