30939285a0425047e010d5abb9c14fe9b07d78b0
reza <reza@pop-os.localdomain>
2023-12-03 21:35:28 +0700

    add loading  page

--- app/page.js
+++ app/page.js
@@ -45,78 +45,78 @@ function page() {
   }, []);
   return (
     <div>
-      {/* {isLoading ? (
+      {isLoading ? (
         <ProgressBar progress={progress} />
-      ) : ( */}
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 0.5 }}
-        className="relative"
-      >
-        <section>
-          <Image
-            width={700}
-            height={3499}
-            alt="banner euy"
-            src="/banner.png"
-            className="w-full md:hidden h-auto absolute top-0  left-0 -z-10"
-          />
-          <div className=" translate-y-[14vh] md:translate-y-[5vh] md:m-auto lg:w-[60%]">
-            <motion.div
-              initial={{ opacity: 0, y: -50 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.7 }}
-              className="flex items-center flex-row md:flex md:flex-col"
-            >
-              <Image
-                width={100}
-                height={10}
-                alt="Foto Ku Jir"
-                src="/Profile_pict.png"
-                className="md:w-52 md:h-52"
-              />
-              <div className="ml-4 md:m-auto md:mt-6 md:flex md:flex-col md:justify-center">
-                <p className="text-white text-2xl md:text-5xl font-bold md:text-center">
-                  DEK REZA
-                </p>
-                <div className="flex flex-row md:m-auto md:mt-4">
-                  <p class="py-1 px-2 whitespace-nowrap rounded-full bg-purple text-white font-bold text-xs md:text-xl md:py-2 md:px-4">
-                    Jr. Designer
+      ) : (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5 }}
+          className="relative"
+        >
+          <section>
+            <Image
+              width={700}
+              height={3499}
+              alt="banner euy"
+              src="/banner.png"
+              className="w-full md:hidden h-auto absolute top-0  left-0 -z-10"
+            />
+            <div className=" translate-y-[14vh] md:translate-y-[5vh] md:m-auto lg:w-[60%]">
+              <motion.div
+                initial={{ opacity: 0, y: -50 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.7 }}
+                className="flex items-center flex-row md:flex md:flex-col"
+              >
+                <Image
+                  width={100}
+                  height={10}
+                  alt="Foto Ku Jir"
+                  src="/Profile_pict.png"
+                  className="md:w-52 md:h-52"
+                />
+                <div className="ml-4 md:m-auto md:mt-6 md:flex md:flex-col md:justify-center">
+                  <p className="text-white text-2xl md:text-5xl font-bold md:text-center">
+                    DEK REZA
                   </p>
-                  <p class="py-1 px-2 whitespace-nowrap ml-2 bg-pink rounded-full text-white font-bold text-xs md:text-xl md:py-2 md:px-4">
-                    Jr. Developer
-                  </p>
+                  <div className="flex flex-row md:m-auto md:mt-4">
+                    <p class="py-1 px-2 whitespace-nowrap rounded-full bg-purple text-white font-bold text-xs md:text-xl md:py-2 md:px-4">
+                      Jr. Designer
+                    </p>
+                    <p class="py-1 px-2 whitespace-nowrap ml-2 bg-pink rounded-full text-white font-bold text-xs md:text-xl md:py-2 md:px-4">
+                      Jr. Developer
+                    </p>
+                  </div>
                 </div>
+              </motion.div>
+              <motion.div
+                initial={{ opacity: 0, x: -50 }}
+                animate={{ opacity: 1, x: 0 }}
+                transition={{ duration: 0.7, delay: 0.3 }}
+                className="mt-4 md:mt-7 md:max-w-4xl md:mx-auto md:text-center"
+              >
+                <p className="text-white hidden md:flex  text-2xl">{text}</p>
+                <p className="text-white  mobile-subtitle  md:hidden overflow-hidden">
+                  {truncatedText}
+                  <button
+                    onClick={toggleTruncate}
+                    className="ml-1 text-purple cursor-pointer"
+                  >
+                    {truncated ? "More" : "Less"}
+                  </button>
+                </p>
+              </motion.div>
+              <Socmed />
+              <DesktopSocmed />
+              <div className="mt-4">
+                <HomePortfolio />
+                <HomeGuide />
               </div>
-            </motion.div>
-            <motion.div
-              initial={{ opacity: 0, x: -50 }}
-              animate={{ opacity: 1, x: 0 }}
-              transition={{ duration: 0.7, delay: 0.3 }}
-              className="mt-4 md:mt-7 md:max-w-4xl md:mx-auto md:text-center"
-            >
-              <p className="text-white hidden md:flex  text-2xl">{text}</p>
-              <p className="text-white  mobile-subtitle  md:hidden overflow-hidden">
-                {truncatedText}
-                <button
-                  onClick={toggleTruncate}
-                  className="ml-1 text-purple cursor-pointer"
-                >
-                  {truncated ? "More" : "Less"}
-                </button>
-              </p>
-            </motion.div>
-            <Socmed />
-            <DesktopSocmed />
-            <div className="mt-4">
-              <HomePortfolio />
-              <HomeGuide />
             </div>
-          </div>
-        </section>
-      </motion.div>
-      {/* )} */}
+          </section>
+        </motion.div>
+      )}
     </div>
   );
 }
