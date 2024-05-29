8b9c5cd386355bce21384d8f4c2ed0b6b82625ae
Akbar Fahreza <66171523+AkbarFahreza@users.noreply.github.com>
2024-05-29 12:08:14 +0700

    Status : :Changing Comms status

--- components/comms-info/CommsYTLC/YTLC_CommsPage.jsx
+++ components/comms-info/CommsYTLC/YTLC_CommsPage.jsx
@@ -16,8 +16,8 @@ function YTLC_CommsPage() {
       <div className="flex flex-row justify-between items-center  my-6">
         <p className="mobile-heading-b text-white">
           Status :{" "}
-          <span className="text-red-500">
-            Closed until June
+          <span className="text-yellow-500">
+            Selective
           </span>
         </p>
         <div className="flex flex-row space-x-3">
